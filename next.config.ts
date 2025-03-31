fa63900d6f7234e72574f6fbe1711b0a2319f266
r38p6 <r38p6@hhhuuu.com>
2025-03-31 14:18:53 +0000

    output export

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,17 @@
 import type { NextConfig } from 'next'
  
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      // Basic redirect
-      {
-        source: '/',
-        destination: '/products/',
-        permanent: true,
-      },
-    ]
-  },
+  output:"export",
+  // async redirects() {
+  //   return [
+  //     // Basic redirect
+  //     {
+  //       source: '/',
+  //       destination: '/products/',
+  //       permanent: true,
+  //     },
+  //   ]
+  // },
 }
  
 export default nextConfig
