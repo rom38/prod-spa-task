db1e6b0263a56f169b207310650614b6a47f219f
r38p6 <r38p6@hhhuuu.com>
2025-03-31 13:35:34 +0000

    small fix basepath

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,18 @@ import type { NextConfig } from 'next'
  
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/prod-spa-task',
-  async redirects() {
-    return [
-      // Basic redirect
-      {
-        source: '/',
-        destination: '/products/',
-        //destination: '/',
-        permanent: true,
-      },
-    ]
-  },
+  // basePath: '/prod-spa-task',
+  // async redirects() {
+  //   return [
+  //     // Basic redirect
+  //     {
+  //       source: '/',
+  //       destination: '/products/',
+  //       //destination: '/',
+  //       permanent: true,
+  //     },
+  //   ]
+  //},
 }
  
 export default nextConfig
